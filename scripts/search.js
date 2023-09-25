bd27b5fa2ca388569ddb901d206e5b9b3905ebbc
Vitaliy Tarassov <apxustudio@gmail.com>
2023-09-25 17:51:07 +0600

    header - ui-kid done, search popup done

---
+++ scripts/search.js
@@ -0,0 +1,20 @@
+// const open = document.querySelector('#search-open')
+// const searchForm = document.querySelector('#search-form')
+// const close = document.querySelector('#search-close')
+
+// open.addEventListener('click', function() {
+//   searchForm.classList.add('search-form_show')
+// })
+
+// close.addEventListener('click', function() {
+//   searchForm.classList.remove('search-form_show')
+// })
+document.addEventListener('DOMContentLoaded', (e) => {
+  document.getElementById('search-open').addEventListener('click', (e) => {
+    document.getElementById('search-form').classList.add('search-form_show')
+  })
+
+  document.getElementById('search-close').addEventListener('click', (e) => {
+    document.getElementById('search-form').classList.remove('search-form_show')
+  })
+})
