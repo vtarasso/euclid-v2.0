cb3096a8a666c0a84cd977cdc03524798a276281
Vitaliy Tarassov <apxustudio@gmail.com>
2023-09-27 18:32:11 +0600

    adaptive - 1024pxp done

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('burger').addEventListener('click', function(){
-    document.querySelector('header').classList.toggle('open')
+  const burger = document.querySelector('#burger')
+  const menuLinks = document.querySelectorAll('.nav__link')
+  const header = document.querySelector('.header')
+
+  burger.addEventListener('click', () => {
+    header.classList.toggle('open')
   })
+
+  menuLinks.forEach((link) =>
+    link.addEventListener('click', () => {
+      header.classList.remove('open')
+    })
+  )
+
+  // document.getElementById('burger').addEventListener('click', function(){
+  //   document.querySelector('header').classList.toggle('open')
+  // })
 })
